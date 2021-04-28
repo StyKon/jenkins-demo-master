0fc751aaf9c2ec0b2a315c11bfb37b08f484d1e7
StyKon <76910363+StyKon@users.noreply.github.com>
2021-04-28 03:37:58 +0100

    Update createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,43 @@
 pipelineJob('pipelineJob') {
+2
     definition {
+3
         cps {
+4
             script(readFileFromWorkspace('pipelineJob.groovy'))
+5
             sandbox()
+6
         }
+7
     }
+8
+}
+9
+pipelineJob('theme-park-job') {
+10
+    definition {
+11
+        cpsScm {
+12
+            scm {
+13
+                git {
+14
+                    remote {
+15
+                        url 'https://github.com/StyKon/SpringBootLocationVoiture.git'
+16
+                    }
+17
+                    branch 'master'
+18
+                }
+19
+            }
+20
+        }
+21
+    }
+22
 }
